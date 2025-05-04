6470c8ec705877902c3b95aade69fb66d85a0b33
Gikai Andrews <gnaboys@gmail.com>
2025-05-04 18:55:36 -0400

    updated font for homepage, finished contact page, deleted old font and added contact background

--- bman/src/Contact/Contact.jsx
+++ bman/src/Contact/Contact.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
+import contactbg from '../images/contact-bg.jpg';
 
 function Contact() {
   return (
-    <div className='contact'>
-            <form className='contact-form'>
-                <label  htmlFor='email' className='contact-label'>Your Email:</label>
-                <input type='text' placeholder='person@example.com' className='input-field'></input>
-                <label htmlFor='message' className='contact-label'>Message:</label>
-                <textarea name='message' className='message-box' placeholder='Message...'></textarea>
-                <br></br>
-                <button type='submit'className='submit-button'>Send</button>
-            </form>
-        </div>
-  )
+    <>
+      <div className="contact-bg-wrapper">
+      <img src={contactbg} alt="editing-bg" className="contact-bg"></img>
+      </div>
+      <div className="contact">
+        <form className="contact-form">
+          <label htmlFor="email" className="contact-label">
+            Your Email:
+          </label>
+          <input
+            type="text"
+            placeholder="person@example.com"
+            className="input-field"
+          ></input>
+          <label htmlFor="message" className="contact-label">
+            Message:
+          </label>
+          <textarea
+            name="message"
+            className="message-box"
+            placeholder="Message..."
+          ></textarea>
+          <br></br>
+          <button type="submit" className="submit-button">
+            Send
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
