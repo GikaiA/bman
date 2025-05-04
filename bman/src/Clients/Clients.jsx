fa0ae465fc851bc62ff008dd7570d2b0536a277b
Gikai Andrews <gnaboys@gmail.com>
2025-05-04 13:24:21 -0400

    added background to homepage, clients page in progress

--- bman/src/Clients/Clients.jsx
+++ bman/src/Clients/Clients.jsx
@@ -1,8 +1,30 @@
-import React,{useState}  from 'react';
-import './Clients.css';
-import brooke from '../images/brooke-logo.png';
-import tibackground from '../images/ti-background.jpg'
-
+import React, { useState } from "react";
+import "./Clients.css";
+import brooke from "../images/brooke-logo.png";
+import tibackground from "../images/ti-background.jpg";
+import tilogo from "../images/ti-logo.png";
+import tividsample from "../videos/tiwz-clip.mp4";
+import ticlaymore from "../videos/claymore.mp4";
+import xaphora1 from "../videos/xaphora1.mp4";
+import xaphora2 from "../videos/xaphora2.mp4";
+import xaphora3 from "../videos/xaphora3.mp4";
+import zuno1 from "../videos/zuno1.mp4";
+import zuno2 from "../videos/zuno2.mp4";
+import zuno3 from "../videos/zuno3.mp4";
+import brooke1 from "../videos/brooke1.mp4";
+import brooke2 from "../videos/brooke2.mp4";
+import brooke3 from "../videos/brooke3.mp4";
+import queen1 from "../videos/queendee1.mp4";
+import queen2 from "../videos/queendee2.mp4";
+import queen3 from "../videos/queendee3.mp4";
+import kea1 from "../videos/kea1.mp4";
+import kea2 from "../videos/kea2.mp4";
+import tiunboxing from "../videos/cod-unboxing.mp4";
+import xaphoralogo from "../images/xaphora-logo.png";
+import zunologo from "../images/zuno-logo.jpg";
+import dequeenlogo from "../images/deequeenlogo.png";
+import brookelogo from "../images/brooke-logo.png";
+import kealogo from "../images/kea-logo.png";
 
 function Clients() {
   const clients = [
@@ -11,7 +33,7 @@ function Clients() {
       name: "Client A",
       description: "This is client A's description.",
       image: brooke,
-      background: tibackground
+      background: tibackground,
     },
     {
       id: 2,
@@ -43,14 +65,14 @@ function Clients() {
   return (
     <div className="client-container">
       <div className={`client-content ${fade ? "fade-in" : "fade-out"}`}>
-        <img src={client.image} alt={client.name} className='client-image' />
+        <img src={client.image} alt={client.name} className="client-image" />
 
         <p>{client.description}</p>
-        <img src={client.background} className='background-image'/>
+        <img src={client.background} className="background-image" />
       </div>
       <button onClick={handleNext}>Next</button>
     </div>
   );
 }
 
-export default Clients
+export default Clients;
--- bman/src/Home/Home.jsx
+++ bman/src/Home/Home.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
+import videobg from "../images/editing-bg.jpg";
 
 function Home() {
   return (
-    <div className='home'>
-      <div className='heading-section'>
-        <h1 className='heading-title'> Welcome to Bman Productions</h1>
-        <p className='portfolio-paragraph'>
+    <div className="home">
+      <div className="bg-wrapper">
+        <img src={videobg} alt="editing-bg" className="editing-bg"></img>
+      </div>
+      <div className="heading-section">
+        <h1 className="heading-title"> Welcome to Bman Productions</h1>
+        <p className="portfolio-paragraph">
           Home of the Miami-based Video Editor
         </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
